cc5164a6e5bcf8d24bb57552c2278329c1819acc
q66 <daniel@octaforge.org>
2020-04-26 19:09:28 +0200

    reduce gamecode to a single file

--- src/game/game.cc
+++ src/game/game.cc
@@ -1,26 +1,16 @@
-#include "game.hh"
+#include "cube.hh"
 
 extern void clearmainmenu();
 
 namespace game
 {
     dynent *player1 = NULL;         // our client
+    string clientmap = "";
+    bool connected = false;
 
     const char *getclientmap() { return clientmap; }
 
     const char *gameident() { return "OctaForge"; }
-
-    void saveragdoll(dynent *d)
-    {
-    }
-
-    void clearragdolls()
-    {
-    }
-
-    void moveragdolls()
-    {
-    }
 
     void rendergame()
     {
@@ -40,7 +30,6 @@ namespace game
 
     void preload()
     {
-        entities::preloadentities();
     }
 
     void resetgamestate()
@@ -77,7 +66,6 @@ namespace game
         if(!curtime) { return; }
 
         physicsframe();
-        moveragdolls();
         if(connected)
         {
             crouchplayer(player1, 10, true);
@@ -224,8 +212,6 @@ namespace game
     {
     }
 
-    bool connected = false;
-
     void writeclientinfo(stream *f)
     {
     }
@@ -239,8 +225,6 @@ namespace game
     {
         disablezoom();
     }
-
-    string clientmap = "";
 
     void changemap(const char *name)
     {
@@ -303,7 +287,86 @@ namespace game
         }
     }
 
-    void toserver(char *text) { conoutf(CON_CHAT, "%s", text); }
+    void toserver(char *text) { }
+}
+
+namespace entities
+{
+    using namespace game;
+
+    int extraentinfosize() { return 0; }       // size in bytes of what the 2 methods below read/write... so it can be skipped by other games
+
+    void writeent(entity &e, char *buf)   // write any additional data to disk (except for ET_ ents)
+    {
+    }
+
+    void readent(entity &e, char *buf, int ver)     // read from disk, and init
+    {
+    }
+
+    vector<extentity *> ents;
+
+    vector<extentity *> &getents() { return ents; }
+
+    bool mayattach(extentity &e) { return false; }
+    bool attachent(extentity &e, extentity &a) { return false; }
+
+    const char *entmodel(const entity &e)
+    {
+        return NULL;
+    }
+
+    void preloadentities()
+    {
+    }
+
+    void renderentities()
+    {
+    }
+
+    extentity *newentity() { return new extentity(); }
+    void deleteentity(extentity *e) { delete e; }
+
+    void clearents()
+    {
+        while(ents.length()) deleteentity(ents.pop());
+    }
+
+    void animatemapmodel(const extentity &e, int &anim, int &basetime)
+    {
+    }
+
+    void fixentity(extentity &e)
+    {
+    }
+
+    void entradius(extentity &e, bool color)
+    {
+    }
+
+    bool printent(extentity &e, char *buf, int len)
+    {
+        return false;
+    }
+
+    const char *entnameinfo(entity &e) { return ""; }
+    const char *entname(int i)
+    {
+        static const char * const entnames[ET_GAMESPECIFIC] =
+        {
+            "none?", "light", "mapmodel", "playerstart", "envmap", "particles", "sound", "spotlight", "decal",
+        };
+        return i>=0 && size_t(i)<sizeof(entnames)/sizeof(entnames[0]) ? entnames[i] : "";
+    }
+
+    void editent(int i, bool local)
+    {
+    }
+
+    float dropheight(entity &e)
+    {
+        return 4.0f;
+    }
 }
 
 bool haslocalclients() { return game::connected; }
