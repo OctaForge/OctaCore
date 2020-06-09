2749bda1418afabcd0db4bcbdca27b1e3cdfb498
q66 <daniel@octaforge.org>
2020-06-10 01:48:35 +0200

    remove bih.hh include from engine.hh

--- src/engine/engine.hh
+++ src/engine/engine.hh
@@ -3,9 +3,7 @@
 
 #include "cube.hh"
 #include "world.hh"
-
 #include "light.hh"
-#include "bih.hh"
 
 extern dynent *player;
 extern physent *camera1;                // special ent that acts as camera, same object as player1 in FPS mode
