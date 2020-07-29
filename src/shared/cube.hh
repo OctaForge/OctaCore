219bea20be588231f68ceae230c9d12ad21fbb25
q66 <daniel@octaforge.org>
2020-07-29 05:00:15 +0200

    remove standard includes from cube.hh

--- src/shared/cube.hh
+++ src/shared/cube.hh
@@ -1,12 +1,6 @@
 #ifndef __CUBE_H__
 #define __CUBE_H__
 
-#include <math.h>
-
-#include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
-
 #include "tools.hh"
 #include "geom.hh"
 #include "ents.hh"
